37817226a5ea1b79ad8d09cf96c4b1da7e1b102d
Vitaliy Shahanyants <vitaliys@clearcable.ca>
2019-11-27 11:13:31 -0500

    Adding Score and Title Fragments, and setting Navigation and auto generated direction classes by safeargs

--- app/src/main/java/ca/vitos/guesstheword/views/score/ScoreFragment.kt
+++ app/src/main/java/ca/vitos/guesstheword/views/score/ScoreFragment.kt
@@ -1,6 +1,25 @@
 package ca.vitos.guesstheword.views.score
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import ca.vitos.guesstheword.R
+import ca.vitos.guesstheword.databinding.ScoreFragmentBinding
 
 class ScoreFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding: ScoreFragmentBinding = DataBindingUtil.inflate(inflater, R.layout.score_fragment,container,false)
+
+
+
+        return binding.root
+    }
 }
--- app/src/main/java/ca/vitos/guesstheword/views/title/TitleFragment.kt
+++ app/src/main/java/ca/vitos/guesstheword/views/title/TitleFragment.kt
@@ -1,6 +1,30 @@
 package ca.vitos.guesstheword.views.title
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import ca.vitos.guesstheword.R
+import ca.vitos.guesstheword.databinding.TitleFragmentBinding
 
 class TitleFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val binding: TitleFragmentBinding = DataBindingUtil.inflate(inflater,R.layout.title_fragment,container,false)
+
+        binding.playGameButton.setOnClickListener {
+            findNavController().navigate(TitleFragmentDirections.actionTitleFragmentToGameFragment())
+        }
+
+        return binding.root
+
+    }
 }
